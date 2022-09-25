a1b36fea13975a015303ce1a507094e27a4ae8d7
Oğuz Berk Açar <94904336+oguzberkacar@users.noreply.github.com>
2022-09-25 21:16:33 +0300

    fb fix

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import 'firebase/compat/firestore'
 import 'firebase/compat/auth'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDaEL1cvUETq_nISlebvQJmwgvJW1Z0AUA',
-  authDomain: 'linkedin-clone-oba.firebaseapp.com',
-  projectId: 'linkedin-clone-oba',
-  storageBucket: 'linkedin-clone-oba.appspot.com',
-  messagingSenderId: '453162394064',
-  appId: '1:453162394064:web:060d52b0d68d30017359d5',
+  apiKey: process.env.NEXT_PUBLIC_APIKEY,
+	authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+	projectId: process.env.NEXT_PUBLIC_PROJECTID,
+	storageBucket: process.env.NEXT_PUBLIC_STORAGEBUCKET,
+	messagingSenderId: process.env.NEXT_PUBLIC_MESSAGINGSENDERID,
+	appId: process.env.NEXT_PUBLIC_APPID,
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
